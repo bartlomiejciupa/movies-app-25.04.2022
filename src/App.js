38cf360ac8b2455a24a010fb8b67491a12a09d56
Bartlomiej Ciupa <bartek-ciupa@o2.pl>
2022-04-25 15:52:29 +0200

    Revert "First commit, components added"
    This reverts commit e9b848caf3f62148171513f10197fb448748a2f2.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,24 @@
-import { onAuthStateChanged } from 'firebase/auth';
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import logo from './logo.svg';
 import './App.css';
-import Dashboard from './components/Dashboard';
-import Home from './components/Home';
-import InputMovie from './components/InputMovie';
-import Login from './components/Login';
-import Register from './components/Register';
-import { auth } from './db';
 
 function App() {
-  const [isAuth, setIsAuth] = useState(false);
-
-  onAuthStateChanged(auth, (user) => {
-if (user) {
-  setIsAuth(user.email);
-} else {
-  setIsAuth(false)
-}
-  
-  })
-
   return (
-    <BrowserRouter>
-    <Routes>
-     <Route path="/" element={isAuth ? <Navigate to="/dashboard"/> : <Home/>}/>
-     <Route path="dashboard" element={!isAuth ? <Navigate to="/"/> : <Dashboard user={isAuth} />}/>
-     <Route path="login" element={isAuth ? <Navigate to="/dashboard"/> : <Login />}/>
-     <Route path="input-movie" element={isAuth ? <Navigate to="input-movie" /> : <Home/>}/>
-     <Route path="register" element={isAuth ? <Navigate to="/"/> : <Register/>}/>
-     </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
